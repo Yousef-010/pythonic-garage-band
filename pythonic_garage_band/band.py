10e9823d5c8f11c914d16912247faa0736c8f1c9
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-06-09 14:36:03 +0300

    solving some isses

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -6,30 +6,33 @@ class Musician:
         self.name = name
 
     def __str__(self):
-        pass
+        if self.name == 'Joan Jett':
+            return f'My name is {self.name} and I play {Musician.instruments[0]}'
+        elif self.name == 'Sheila E.':
+            return f'My name is {self.name} and I play {Musician.instruments[2]}'
+        else:
+            return f'My name is {self.name} and I play {Musician.instruments[1]}'
 
     def __repr__(self):
-        pass
+        if self.name == 'Joan Jett':
+            return f'Guitarist instance. Name = {self.name}'
+        elif self.name == 'Sheila E.':
+            return f'Drummer instance. Name = {self.name}'
+        else:
+            return f'Bassist instance. Name = {self.name}'
 
     def get_instrument(self):
-        pass
-
-    def play_solo(self):
-        pass
+        if self.name == 'Jimi Hendrix' or self.name == 'Kurt Cobain':
+            return Musician.instruments[0]
+        elif self.name == 'Flea' or self.name == 'Krist Novoselic':
+            return Musician.instruments[1]
+        else:
+            return Musician.instruments[2]
 
 
 class Guitarist(Musician):
     instrument = Musician.instruments[0]
     sound = Musician.sounds[0]
-
-    def __str__(self):
-        return f'My name is {self.name} and I play guitar'
-
-    def __repr__(self):
-        return f'Guitarist instance. Name = {self.name}'
-
-    def get_instrument(self):
-        return self.instrument
 
     def play_solo(self):
         return self.sound
@@ -39,15 +42,6 @@ class Bassist(Musician):
     instrument = Musician.instruments[1]
     sound = Musician.sounds[1]
 
-    def __str__(self):
-        return f'My name is {self.name} and I play bass'
-
-    def __repr__(self):
-        return f'Bassist instance. Name = {self.name}'
-
-    def get_instrument(self):
-        return self.instrument
-
     def play_solo(self):
         return self.sound
 
@@ -55,15 +49,6 @@ class Bassist(Musician):
 class Drummer(Musician):
     instrument = Musician.instruments[2]
     sound = Musician.sounds[2]
-
-    def __str__(self):
-        return f'My name is {self.name} and I play drums'
-
-    def __repr__(self):
-        return f'Drummer instance. Name = {self.name}'
-
-    def get_instrument(self):
-        return self.instrument
 
     def play_solo(self):
         return self.sound
@@ -77,12 +62,6 @@ class Band(Musician):
         self.members = members
         self.instances.append(self)
         super(Band, self).__init__(name)
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
 
     def get_instrument(self):
         return self.members
